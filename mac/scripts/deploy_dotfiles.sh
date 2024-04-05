e013d436e9c8f749536eba5c159a9db38355c21c
nekowasabi <nolifeking00@gmail.com>
2024-04-05 18:12:40 +0900

    chore(mac): パスのエスケープ処理を修正

--- mac/scripts/deploy_dotfiles.sh
+++ mac/scripts/deploy_dotfiles.sh
@@ -4,7 +4,7 @@ cd ~/repos/dotfiles/vim_integrate
 cp -rfv coc-settings.json init.vim rc mysnippet syntax ~/.config/nvim
 cd ~/repos/dotfiles
 cp -rfv ./zeno/* ~/.config/zeno
-cp -rfv ~/repos/dotfiles/lazygit/* "~/Library/Application Support/lazygit"
+cp -rfv ~/repos/dotfiles/lazygit/* ~/Library/Application\ Support/lazygit
 
 # 引数がprivateの場合はprivateディレクトリにコピー
 if [ "$1" = "private" ]; then
