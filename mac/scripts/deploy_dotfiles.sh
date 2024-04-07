aa1bc7d2f3b93c6d172dc5623a46729b9879d9b5
nekowasabi <nolifeking00@gmail.com>
2024-04-07 19:00:00 +0900

    feat: wsl用のzshrcとwezterm.luaをコピー

--- mac/scripts/deploy_dotfiles.sh
+++ mac/scripts/deploy_dotfiles.sh
@@ -4,15 +4,20 @@ cd ~/repos/dotfiles/vim_integrate
 cp -rfv coc-settings.json init.vim rc mysnippet syntax ~/.config/nvim
 cd ~/repos/dotfiles
 cp -rfv ./zeno/* ~/.config/zeno
-cp -rfv ~/repos/dotfiles/lazygit/* ~/Library/Application\ Support/lazygit
 
 # 引数がprivateの場合はprivateディレクトリにコピー
 if [ "$1" = "private" ]; then
+  cp -rfv ~/repos/dotfiles/lazygit/* ~/Library/Application\ Support/lazygit
   cp -rfv ~/repos/dotfiles/zsh/mac_zshrc ~/.zshrc 
   cp -rfv ~/repos/dotfiles/mac/mac_wezterm.lua ~/.wezterm.lua 
   cp -rfv ~/repos/dotfiles/mac/scripts/* ~/scripts
 
   cp -rfv ~/repos/dotfiles/mac/superwhisper/* ~/Documents/superwhisper
+fi
+
+if [ "$1" = "wsl" ]; then
+  cp -rfv ~/repos/dotfiles/zsh/wsl_zshrc ~/.zshrc 
+  cp -rfv ~/repos/dotfiles/mac/wsl_wezterm.lua ~/.wezterm.lua 
 fi
 
 cd ~/
