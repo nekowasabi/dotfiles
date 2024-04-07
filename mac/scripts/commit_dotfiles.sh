94bf464ddce0ffe34d36cd6a135ee56d79e4d41f
nekowasabi <nolifeking00@gmail.com>
2024-04-07 18:57:26 +0900

    feat: dotfilesにWSL用の設定を追加

--- mac/scripts/commit_dotfiles.sh
+++ mac/scripts/commit_dotfiles.sh
@@ -2,16 +2,21 @@
 
 cd ~/.config/nvim && cp -rfv coc-settings.json init.vim rc mysnippet syntax ~/repos/dotfiles/vim_integrate
 cp -rfv ~/.config/zeno/* ~/repos/dotfiles/zeno
-cp -rfv ~/Library/Application Support/lazygit/* ~/repos/dotfiles/lazygit
 
 # 引数がprivateの場合はprivateディレクトリにコピー
 if [ "$1" = "private" ]; then
+  cp -rfv ~/Library/Application Support/lazygit/* ~/repos/dotfiles/lazygit
   cp -rfv ~/.zshrc ~/repos/dotfiles/zsh/mac_zshrc
   cp -rfv ~/.wezterm.lua ~/repos/dotfiles/mac/mac_wezterm.lua
   cp -rfv ~/scripts ~/repos/dotfiles/mac/
 
   cd ~/Documents/superwhisper
   cp -rfv models settings ~/repos/dotfiles/mac/superwhisper
+fi
+
+if [ "$1" = "wsl" ]; then
+  cp -rfv ~/.zshrc ~/repos/dotfiles/zsh/wsl_zshrc
+  cp -rfv ~/.wezterm.lua ~/repos/dotfiles/mac/wsl_wezterm.lua
 fi
 
 cd ~/repos/dotfiles
