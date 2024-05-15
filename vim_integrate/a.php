4d70afc32cf895eef84436a7d5addf61732dc60b
nekowasabi <nolifeking00@gmail.com>
2024-05-15 10:27:49 +0900

    fix:

--- vim_integrate/a.php
+++ vim_integrate/a.php
@@ -3,6 +3,20 @@
 class A
 {
     /**
+     * Calculate Fibonacci number
+     */
+    public function fibonacci(int $n): int
+    {
+        if ($n <= 0) {
+            return 0;
+        } elseif ($n === 1) {
+            return 1;
+        } else {
+            return $this->fibonacci($n - 1) + $this->fibonacci($n - 2);
+        }
+    }
+
+    /**
      * hello
      */
     public function aaa(): void
@@ -11,4 +25,5 @@ class A
     }
 }
 $obj = new A();
+echo $obj->fibonacci(10); // Example usage of fibonacci method
 $obj->aaa();
