ce90f324bd64968f3f4696393219eb4bfb8d88d3
nekowasabi <nolifeking00@gmail.com>
2026-03-12 17:05:17 +0900

    chore(zsh): pnpmのPATH設定を追加

--- zsh/entry/work.zsh
+++ zsh/entry/work.zsh
@@ -34,3 +34,11 @@ sup() {
     rm -f /tmp/sup-selection
   fi
 }
+
+# pnpm
+export PNPM_HOME="/Users/ttakeda/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
